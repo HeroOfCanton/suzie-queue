36d4fb9e84a40f03dcb9f83ffd9068bb1eaaedbd
Zane Zak <zz@eng.utah.edu>
2017-11-14 11:52:44 -0700

    Change in how courses are defined. ZZ

--- model/config.php
+++ model/config.php
@@ -1,13 +1,28 @@
 <?php
-  define("LDAP_SERVER", "smith.eng.utah.edu");
-  define("LDAP_DOMAIN", "users.coe.utah.edu");
-  define("BIND_USER",   "ta-queue");
-  define("BIND_PASSWD", "Grad4ation18");
-  define("BASE_OU",     "DC=users,DC=coe,DC=utah,DC=edu");
-  define("COURSE_OU",   "OU=ENG Class Groups OU,OU=ENG OU,DC=users,DC=coe,DC=utah,DC=edu");
+define("LDAP_SERVER", "smith.eng.utah.edu");
+define("LDAP_DOMAIN", "users.coe.utah.edu");
+define("BIND_USER",   "ta-queue");
+define("BIND_PASSWD", "Grad4ation18");
+define("BASE_OU",     "DC=users,DC=coe,DC=utah,DC=edu");
+define("COURSE_OU",   "OU=ENG Class Groups OU,OU=ENG OU,DC=users,DC=coe,DC=utah,DC=edu");
 
-  define("SQL_SERVER",  "localhost");
-  define("SQL_USER",    "FIXME");
-  define("SQL_PASSWD",  "FIXME");
-  define("DATABASE",    "FIXME");
+define("SQL_SERVER",  "localhost");
+define("SQL_USER",    "FIXME");
+define("SQL_PASSWD",  "FIXME");
+define("DATABASE",    "FIXME");
+
+
+
+/*
+ *This maps a course name to its sAMAccountName LDAP
+ *
+ *To add a new course to the queue, simply create a group
+ *in LDAP for it in $COURSE_OU, and add it to the mapping
+ *
+ *Course_Name => group_sAMAccountName
+ */ 
+$courses_avail = array(
+  "CS 9999" => "cs9999",
+);
+  
 ?>
