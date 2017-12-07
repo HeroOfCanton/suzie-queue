a75e931c7b3a87782d4a079d7cd90f0a3dbde5ea
HeroOfCanton <ryan@smugmug.com>
2017-12-06 23:57:01 -0700

    routing on api/login, begin table on my_classes, and try to fix forEach is not a function error on classes.js

--- view/src/classes.js
+++ view/src/classes.js
@@ -10,7 +10,7 @@ function renderCourse(course) {
 function renderCourseTable(courses) {
     // #6
     var table = $('#all_classes');
-    courses.forEach(function (each_class) {
+    courses.forEach(function (each_class.all_courses) {
         // #7
         var renderedCourse = renderCourse(each_class);
         // #8
@@ -48,3 +48,14 @@ $(function () {
         });
     */
 });
+
+
+// 1. Server returns all_classes_view.php
+// 1.1 Browser parses the markup, creating the Document Object Model (DOM). This will contain a DOM "node" that had the id "classes-table" (or whatever it is).
+// 2. Browser sees script tag for 'src/classes.js', requests that asynchronously
+// 3. This script runs, declaring (but not calling) the renderCourse and renderCourseTable functions. It also calls $.ready, providing a function to run WHEN THE DOCUMENT IS FINISHED LOADING (not at the time $.ready is called).
+// 4. Document finishes loading, $.ready callback is executed making the $.ajax call, making an asynchronous request to the backend API endpoint
+// 5. Server processes the request and sends a response, triggering the callback that was provided to the $.ajax call through the 'success' parameter. This calls renderCourseTable w/ the JSON that the AJAX call received.
+// 6. renderCourseTable grabs the DOM node for the table begins looping over each class.
+// 7. For each class, renderCourse is called, creating a NEW DOM node ("tr"), appending the text data as needed, and returning the DOM node. Note that this is a "detached" DOM node in that it has not yet been inserted into the DOM "tree" and thus not yet rendered.
+// 8. When renderCourse returns, renderCourseTable will insert it into the table, rendering it on the page because the table DOM node was already attached to the rendered DOM tree.
