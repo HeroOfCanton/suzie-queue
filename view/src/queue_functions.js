19e692e28fb4412e4792564a400570d1e9e453f9
Zane Zak <zz@eng.utah.edu>
2018-01-22 09:47:27 -0700

    Queue is pulled and displayed Refreshed every five seconds
    Zz

--- view/src/queue_functions.js
+++ view/src/queue_functions.js
@@ -8,22 +8,42 @@ for (var i=0;i<vars.length;i++) {
     break;
   }
 }
-get_queue(course);
 is_TA = false;
+get_queue(course);
+setInterval(function(){get_queue(course);}, 5000);
 
 function get_queue(course) {
+  $("#queue tr").remove();
+  
   var url = "../api/queue/get_queue.php";
   var posting = $.post( url, { course: course } );
- 
+
   var done = function(data) {
     var dataString = JSON.stringify(data);
     var dataParsed = JSON.parse(dataString);
     if(dataParsed.error){
-      alert("Error fetching queue");a
+      alert("Error fetching queue");
       return;
     }
-    
+   
     queue_state = dataParsed.state;
+
+    //Renders the queue table
+    if(queue_state == "closed"){
+      $('#queue_table').hide();
+    }else{
+      $('#queue_table').show();
+      $('#queue').show();
+      queue = dataParsed.queue;
+      $('#queue').append("<tr> <th>Student</th> <th>Location</th> <th>Question</th> </tr>");
+      for(row in queue){
+        username = queue[row].username;
+        question = queue[row].question;
+        Location = queue[row].location;
+        $('#queue').append('<tr> <td>'+username+'</td> <td>'+Location+'</td> <td>'+question+'</td> </tr>'); 
+      }
+    }
+
     if(is_TA){
       if(queue_state == "closed"){
         $("#state_button").text("OPEN QUEUE");
@@ -38,6 +58,16 @@ function get_queue(course) {
           close_queue(course);
         });
       }
+    }else{
+      $("#state_button").hide();
+
+      $("#join_button").text("Enter Queue");
+      $("#join_button").show();
+      $("#state_button").click(function( event ) {
+        event.preventDefault();
+        enqueue_student(course);
+      });
+
     }
       
   }
